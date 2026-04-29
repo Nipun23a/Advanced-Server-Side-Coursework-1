3f577bea35ba255291bec4416ad9200aaadac64b
Nipuna Kaweya <nipuna609@gmail.com>
2026-04-29 20:54:35 +0530

    Codeingitor Model Updated

--- codeigniter-app/app/Models/EmploymentHistoryModel.php
+++ codeigniter-app/app/Models/EmploymentHistoryModel.php
@@ -10,7 +10,7 @@ class EmploymentHistoryModel extends Model
     protected $primaryKey = 'id';
     protected $returnType = 'array';
     protected $useAutoIncrement = true;
-    protected $allowedFields = ['profile_id', 'company_name', 'job_title', 'start_date', 'end_date'];
+    protected $allowedFields = ['profile_id', 'company_name', 'industry_sector', 'job_title', 'start_date', 'end_date'];
     protected $useTimestamps = true;
     protected $createdField = 'created_at';
     protected $updatedField = 'updated_at';
